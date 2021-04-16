1b4ab9d4ecff72d8727af4188dc09190d61dbe98
Vinicyus Marques <vinicyusmarquess@gmail.com>
2021-04-16 15:54:23 -0300

    configuraçao de report

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,29 +1,31 @@
-require 'selenium-webdriver'
 require 'report_builder'
+Before do
+    Capybara.current_session.driver.browser.manage.delete_all_cookies
+    window = Capybara.current_session.current_window
+    window.resize_to(1200, 1200)
+    window.maximize
+end
+
+After do |scn|
+    binding.pry if scn.failed? && ENV['debug']
+    # Setting Screenshot
+    temp_shot = page.save_screenshot('results/temp_shot.png')
+    screenshot = Base64.encode64(File.open(temp_shot, 'rb').read)
+    embed(screenshot, 'image/png', 'Screenshot')
+    # Set reset session hook
+    page.execute_script('sessionStorage.clear()')
+    Capybara.current_session.driver.quit
+    @session.driver.quit unless @session.nil?
+end
 
 at_exit do
     ReportBuilder.configure do |config|
       config.input_path = 'results/report.json'
       config.report_path = 'results/report'
-      config.report_types = %i[retry html]
-      config.report_title = 'Resultados do Teste'
-      config.compress_image = true
-      #config.additional_info = { 'App' => 'Web', 'Data de execução' => @current_date }
-      config.color = 'indigo'
+      #config.report_path = 'relatório de teste'
+      config.report_types = %i[html]
+      config.report_title = 'Relatório dos testes'
+      config.additional_info = { 'Navegador' => 'Web' }
     end
     ReportBuilder.build_report
-  end
-
-  Before do
-    Capybara.current_session.driver.browser.manage.delete_all_cookies
-    window = Capybara.current_session.current_window
-    window.resize_to(1200, 1200)
-    window.maximize
-  end
-
-  After do |scn|
-    # Set reset session hook
-    page.execute_script('sessionStorage.clear()')
-    Capybara.current_session.driver.quit
-  end
-
+end
